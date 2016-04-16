66151ba445993122ab5f139e823bd3aada31f963
hwagz <hunt.wagner@gmail.com>
2016-04-16 01:32:15 -0400

    Off-by-one indexing error caused sticky block glitch. Blocks and dropping working as intended now, just need to add actual game/point idea.

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
+  //WARNING: Still in progress. Actual game mechanic not yet implemented. Clicky-fally works though.
+
   /* Goal: A sort of tetris/bejeweled hybrid.
     Every ~3 seconds, a row of 5 blocks is dropped from the top of the gamespace and falls in to fill gaps. Clicked blocks are removed if there is more than one of the same color touching it, and all blocks of that color are removed.
     Each row will need a current block count. Maybe separate thing/object constructor for the blocks?
     Loop with recursive function for adjacent block determination
+
+    THOUGHTS: Scratch the bejeweled BS. .5s timer and color matching to color panel (that DNE yet), deduct points for wrong answers, gets faster,
   */
+
+
 
   var falling = {
     currentBlocks: 0,
-    numRows: 0,
     maxRowSize: 5,
     speed: 750,
     blockObjs: [],
     blockWidth: 100,
     blockHeight: 100,
-    blockInterval: 5000,
+    blockInterval: 3000,
     gameHeight: 400,
     blockCounts: [0,0,0,0,0],
     interval: null,
-    colors: ["red","darkblue","chartreuse","darkorange","fuchsia"],
+    colors: ["red","blue","chartreuse","orange","fuchsia"],
     init: function(){
       this.cacheDom();
+      this.bindEvents();
       this.centerElement(this.$game);
       this.startFalling();
     },
@@ -27,6 +33,12 @@ $(document).ready(function(){
       this.$game = $('.game');
       this.$window = $(window);
       this.$block = $('.block');
+      this.$doc = $(document);
+    },
+    bindEvents: function(){
+      this.$doc.on('click','div',function(){
+        falling.removeBlock(this.id);
+      });
     },
     centerElement: function($el){
       var desiredx = this.$window.width()/2-$el.width()/2;
@@ -46,50 +58,51 @@ $(document).ready(function(){
     },
     fall: function(aBlockObj){
       var fallDist = (falling.gameHeight-(aBlockObj.row*falling.blockHeight));
+      var fallSpeed = falling.speed;
       if (aBlockObj.init) {
         fallDist = falling.blockHeight;
+        fallSpeed = falling.speed/2;
         aBlockObj.row--;
       }
-      console.log(fallDist);
-      $("#"+aBlockObj.id).animate({top: "+="+fallDist+"px"},falling.speed);
+      $("#"+aBlockObj.id).animate({top: "+="+fallDist+"px"},fallSpeed);
     },
     addRow: function(){
       for (var i = 0; i < this.maxRowSize; i++) {
         if (this.blockCounts[i]<4) {
-          this.currentBlocks++;
-          var id = "block"+this.currentBlocks;
-          this.blockObjs.push(this.createBlockObj(id,i,this.blockCounts[i]));
-          var endIndex = falling.blockObjs.length-1;
+          this.currentBlocks++; //increments total blocks
+          var id = "block"+this.currentBlocks; //build block id
+          this.blockObjs.push(this.createBlockObj(id,i,this.blockCounts[i])); //add block obj to array
+          var endIndex = falling.blockObjs.length-1; //only needs calc'd once
           this.$game.append("<div class='block' id='"+id+"'></div>");
-          id="#"+id;
-          var bgColor = this.randomColor();
-          $(id).css({top:-1*this.blockHeight,left:this.blockWidth*i,backgroundColor:bgColor});
+          id="#"+id; //update id for jQ shenanigans
+          var bgColor = this.randomColor(); //get color
+          $(id).css({top:-1*this.blockHeight,left:this.blockWidth*i,backgroundColor:bgColor}); //set position and color
           this.fall(falling.blockObjs[endIndex]);
-          falling.blockObjs[endIndex].init = true;
-          this.blockCounts[i]++;
+          falling.blockObjs[endIndex].init = true; //boolean for later
+          this.blockCounts[i]++; //increments blocks in the specified column
+        }
+      }
+    },
+    dropAboveBlocks: function(index,row,col){
+      for (var i = index+1; i < falling.blockObjs.length; i++) {
+        if (falling.blockObjs[i].col===col && falling.blockObjs[i].row>row) {
+          falling.fall(falling.blockObjs[i]);
         }
       }
     },
     removeBlock: function(id){
-      var colNum;
-      var rowNum;
-      var saveIndex;
+      var colNum, rowNum, saveIndex;
       for (var i = 0; i < falling.blockObjs.length; i++) {
         if (falling.blockObjs[i].id==id) {
           colNum = falling.blockObjs[i].col;
           rowNum = falling.blockObjs[i].row;
           falling.blockObjs.splice(i,1);
-          saveIndex = i;
+          saveIndex = i-1;//save for later, decrement because splice
         }
       }
-      falling.blockCounts[colNum]--;
       $("#"+id).remove();
-      for (var i = saveIndex+1; i < falling.blockObjs.length; i++) {
-        if (falling.blockObjs[i].col===colNum && falling.blockObjs[i].row>rowNum) {
-          //falling.blockObjs[i].row;
-          falling.fall(falling.blockObjs[i]);
-        }
-      }
+      falling.blockCounts[colNum]--;
+      falling.dropAboveBlocks(saveIndex,rowNum,colNum);
     },
     startFalling: function(){
       this.interval = setInterval(function () {
@@ -97,14 +110,10 @@ $(document).ready(function(){
       }, falling.blockInterval);
     },
     stopFalling: function(){
+      // Currently unused, but pause function would be good
       clearInterval(this.interval);
     }
   };
   falling.init();
-
-
-  $(document).on('click','div',function(){
-    falling.removeBlock(this.id);
-  });
 
 }); //end
